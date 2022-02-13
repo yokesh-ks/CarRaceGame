84f175b4921d744b13623f9bb3e876b4ac5ac42c
yokesh-ks <ksyokesh98@gmail.com>
2022-02-13 22:05:18 +0530

    Updated keyPress Event

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
-const score = document.querySelector('.score')
-const startScreen = document.querySelector('.startScreen')
-const gameArea = document.querySelector('.gameArea')
+const score = document.querySelector(".score");
+const startScreen = document.querySelector(".startScreen");
+const gameArea = document.querySelector(".gameArea");
 
 const keys = {
-    ArrowUp: false,
-    ArrowDown: false,
-    ArrowLeft: false,
-    ArrowDown: false,
-}
+  ArrowUp: false,
+  ArrowDown: false,
+  ArrowLeft: false,
+  ArrowRight: false,
+};
 
-const start = () => {console.log("start")}
+const start = () => {
+  console.log("start");
+};
 
 const pressOn = (e) => {
-    console.log(e.key + " is pressed")
-    keys[e.key] = true
-    console.log(keys)
-}
+  console.log(e.key + " is pressed");
+  keys[e.key] = true;
+  console.log(keys);
+};
 
+const pressOff = (e) => {
+  console.log(e.key + " is released");
+  keys[e.key] = false;
+  console.log(keys);
+};  
 
-const pressOff = (e) => {console.log(e.key + " is released")}
-
-startScreen.addEventListener('click', start())
-document.addEventListener('keydown', pressOn)
-document.addEventListener('keyup', pressOff)
+startScreen.addEventListener("click", start());
+document.addEventListener("keydown", pressOn);
+document.addEventListener("keyup", pressOff);
 
